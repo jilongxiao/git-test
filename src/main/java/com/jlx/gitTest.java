372a5796e6d6ccf43215d7713ccf7d6121a81c57
Jgit <1125778496@qq.com>
2022-10-08 18:12:04 +0800

    hot-fix commit2

--- src/main/java/com/jlx/gitTest.java
+++ src/main/java/com/jlx/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
         System.out.println("hello git");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hot-fix test");
     }
 }
